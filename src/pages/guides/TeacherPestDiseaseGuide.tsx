0680f2b12d94931c3e211d3a158e0712dadc4794
boilermanc <boilermanc@gmail.com>
2025-08-26 08:46:59 -0400

    pest update

--- src/pages/guides/TeacherPestDiseaseGuide.tsx
+++ src/pages/guides/TeacherPestDiseaseGuide.tsx
@@ -1,4 +1,4 @@
-// src/pages/guides/TeacherPestDiseaseGuide.tsx - CORRECTED
+// src/pages/guides/TeacherPestDiseaseGuide.tsx - CORRECTEd
 
 import { useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
